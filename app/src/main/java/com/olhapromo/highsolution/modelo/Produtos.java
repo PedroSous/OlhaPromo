3138cfd8934716ea1379805a5c812b024ce9cf53
rafaelllb <rafael.leonardo.brito@gmail.com>
2015-02-18 17:19:05 -0300

    Javadoc adicionado na classe produtos

--- app/src/main/java/com/olhapromo/highsolution/modelo/Produtos.java
+++ app/src/main/java/com/olhapromo/highsolution/modelo/Produtos.java
@@ -26,45 +26,84 @@ public abstract class Produtos {
         this.valor = valor;
     }
 
-
+    /**
+     *
+     * @return retorna o id do produto
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *
+     * @param id altera o id do produto, passando o novo id como parâmetro
+     */
     public void setId(int id) {
         this.id = id;
     }
 
-
+    /**
+     *
+     * @return retorna a marca do produto
+     */
     public String getMarca() {
         return marca;
     }
 
+    /**
+     *
+     * @param marca alera a marca do produto, passando a nova marca como parâmetro
+     */
     public void setMarca(String marca) {
         this.marca = marca;
     }
 
+    /**
+     *
+     * @return retorna a capacidade do processador do produto
+     */
     public String getProcessador() {
+
         return processador;
     }
 
+    /**
+     *
+     * @param processador, altera a capacidade do processador do produto, passando a nova capacidade como parâmetro
+     */
     public void setProcessador(String processador) {
+
         this.processador = processador;
     }
 
+    /**
+     *
+     * @return retorna a memória ram do produto
+     */
     public String getMemoria() {
         return memoria;
     }
 
+    /**
+     *
+     * @param memoria altera a memória ram do produto, passando a nova memória como parâmetro
+     */
     public void setMemoria(String memoria) {
         this.memoria = memoria;
     }
 
-
+    /**
+     *
+     * @return retorna o valor em R$ do produto
+     */
     public String getValor() {
         return valor;
     }
 
+    /**
+     *
+     * @param valor altera o valor em R$ do produto, passando o nov valor como parâmetro
+     */
     public void setValor(String valor) {
         this.valor = valor;
     }
